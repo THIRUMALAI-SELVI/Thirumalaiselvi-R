7d29af56da91eb02159002532a8cfa22dae762ba
THIRUMALAI-SELVI <137981238+THIRUMALAI-SELVI@users.noreply.github.com>
2024-02-22 15:14:14 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,72 +1,29 @@
-const canvas = document.getElementById("canvas");
-const increaseButton = document.getElementById("increase");
-const decreaseButton = document.getElementById("decrease");
-const sizeElement = document.getElementById("size");
-const colorElement = document.getElementById("color");
-const clearElement = document.getElementById("clear");
-const ctx = canvas.getContext("2d");
+const buttonsEl = document.querySelectorAll("button");
 
-let size = 10;
-let color = "black";
-let x;
-let y;
-let isPressed = false;
+const inputFieldEl = document.getElementById("result");
 
-const drawCircle = (x, y) => {
-  ctx.beginPath();
-  ctx.arc(x, y, size, 0, Math.PI * 2);
-  ctx.fillStyle = color;
-  ctx.fill();
-};
+for (let i = 0; i < buttonsEl.length; i++) {
+  buttonsEl[i].addEventListener("click", () => {
+    const buttonValue = buttonsEl[i].textContent;
+    if (buttonValue === "C") {
+      clearResult();
+    } else if (buttonValue === "=") {
+      calculateResult();
+    } else {
+      appendValue(buttonValue);
+    }
+  });
+}
 
-const drawLine = (x1, y1, x2, y2) => {
-  ctx.beginPath();
-  ctx.moveTo(x1, y1);
-  ctx.lineTo(x2, y2);
-  ctx.strokeStyle = color;
-  ctx.lineWidth = size * 2;
-  ctx.stroke();
-};
+function clearResult() {
+  inputFieldEl.value = "";
+}
 
-const updateSizeOnScreen = () => (sizeElement.innerText = size);
+function calculateResult() {
+  inputFieldEl.value = eval(inputFieldEl.value);
+}
 
-canvas.addEventListener("mousedown", (e) => {
-  isPressed = true;
-  x = e.offsetX;
-  y = e.offsetY;
-});
-
-canvas.addEventListener("mouseup", (e) => {
-  isPressed = false;
-  x = undefined;
-  y = undefined;
-});
-
-canvas.addEventListener("mousemove", (e) => {
-  if (isPressed) {
-    x2 = e.offsetX;
-    y2 = e.offsetY;
-    drawCircle(x2, y2);
-    drawLine(x, y, x2, y2);
-    x = x2;
-    y = y2;
-  }
-});
-
-increaseButton.addEventListener("click", () => {
-  size += 5;
-  if (size > 50) size = 50;
-  updateSizeOnScreen();
-});
-
-decreaseButton.addEventListener("click", () => {
-  size -= 5;
-  if (size < 5) size = 5;
-  updateSizeOnScreen();
-});
-
-colorElement.addEventListener("change", (e) => (color = e.target.value));
-
-clearElement.addEventListener("click", () =>
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-);
+function appendValue(buttonValue) {
+  inputFieldEl.value += buttonValue;
+  //   inputFieldEl.value = inputFieldEl.value + buttonValue;
+}
